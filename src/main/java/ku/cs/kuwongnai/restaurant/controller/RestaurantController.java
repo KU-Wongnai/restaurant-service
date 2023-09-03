803af7e297265122d97a139ff905391f321e3260
Pawat-P <pawat.put@ku.th>
2023-09-03 20:11:23 +0700

    Turn separated paths into one nested path

--- src/main/java/ku/cs/kuwongnai/restaurant/controller/RestaurantController.java
+++ src/main/java/ku/cs/kuwongnai/restaurant/controller/RestaurantController.java
@@ -1,12 +1,12 @@
 package ku.cs.kuwongnai.restaurant.controller;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.validation.Valid;
 import ku.cs.kuwongnai.restaurant.entity.Menu;
+import ku.cs.kuwongnai.restaurant.entity.MenuOption;
 import ku.cs.kuwongnai.restaurant.entity.Restaurant;
+import ku.cs.kuwongnai.restaurant.model.MenuOptionRequest;
 import ku.cs.kuwongnai.restaurant.model.MenuRequest;
 import ku.cs.kuwongnai.restaurant.model.RestaurantRequest;
-import ku.cs.kuwongnai.restaurant.service.MenuService;
 import ku.cs.kuwongnai.restaurant.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,11 +22,23 @@ public class RestaurantController {
     @Autowired
     private RestaurantService service;
 
+    @GetMapping
+    public ResponseEntity<List<Restaurant>> getAllRestaurants() {
+        List<Restaurant> restaurants = service.getAllRestaurant();
+        return new ResponseEntity<>(restaurants, HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<Restaurant> create(@Valid @RequestBody RestaurantRequest restaurant) {
         Restaurant createdRestaurant = service.createRestaurant(restaurant);
 
         return new ResponseEntity<>(createdRestaurant, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Restaurant> getRestaurantById(@PathVariable Long id) {
+        Restaurant restaurant = service.getRestaurantById(id);
+        return new ResponseEntity<>(restaurant, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
@@ -41,22 +53,79 @@ public class RestaurantController {
         return new ResponseEntity<>(deletedRestaurant, HttpStatus.OK);
     }
 
-    @PostMapping("/{id}")
+    /* ----------------------- For Menu ----------------------- */
+
+    @GetMapping("/{id}/menu")
+    public ResponseEntity<List<Menu>> getMenus(@PathVariable Long id) {
+        List<Menu> menus = service.getMenus(id);
+        return new ResponseEntity<>(menus, HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}/menu")
     public ResponseEntity<Menu> createMenu(@PathVariable Long id, @Valid @RequestBody MenuRequest menu) {
         Menu createdMenu = service.createMenu(id, menu);
         return new ResponseEntity<>(createdMenu, HttpStatus.CREATED);
     }
 
-    @GetMapping
-    public List<Restaurant> getAllRestaurants() {
-        return service.getAllRestaurant();
+    @GetMapping("/{restaurantId}/menu/items/{menuId}")
+    public ResponseEntity<Menu> getMenuById(@PathVariable Long restaurantId, @PathVariable Long menuId) {
+        Menu menu = service.getMenuById(restaurantId, menuId);
+        return new ResponseEntity<>(menu, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public Restaurant getRestaurantById(@PathVariable Long id) {
-        return service.getRestaurantById(id);
+    @PutMapping("/{restaurantId}/menu/items/{menuId}")
+    public ResponseEntity<Menu> updateMenu(@PathVariable Long restaurantId, @PathVariable Long menuId,
+                                           @Valid @RequestBody MenuRequest menu) {
+        Menu updatedMenu = service.updateMenu(restaurantId, menuId, menu);
+        return new ResponseEntity<>(updatedMenu, HttpStatus.OK);
     }
 
+    @DeleteMapping("/{restaurantId}/menu/items/{menuId}")
+    public ResponseEntity<Menu> deleteMenu(@PathVariable Long restaurantId, @PathVariable Long menuId) {
+        Menu deletedMenu = service.deleteMenu(restaurantId, menuId);
+        return new ResponseEntity<>(deletedMenu, HttpStatus.OK);
+    }
+
+    /* ----------------------- For Menu Option ----------------------- */
+
+    @GetMapping("/{restaurantId}/menu/items/{menuId}/options")
+    public ResponseEntity<List<MenuOption>> getMenuOptions(@PathVariable Long restaurantId,
+                                                           @PathVariable Long menuId) {
+        List<MenuOption> menuOptions = service.getMenuOptions(restaurantId, menuId);
+        return new ResponseEntity<>(menuOptions, HttpStatus.OK);
+    }
+
+    @PostMapping("/{restaurantId}/menu/items/{menuId}/options")
+    public ResponseEntity<MenuOption> createMenuOption(@PathVariable Long restaurantId, @PathVariable Long menuId,
+                                                 @Valid @RequestBody MenuOptionRequest menuOption) {
+        MenuOption createdMenuOption = service.createMenuOption(restaurantId, menuId, menuOption);
+        return new ResponseEntity<>(createdMenuOption, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{restaurantId}/menu/items/{menuId}/options/{menuOptionId}")
+    public ResponseEntity<MenuOption> getMenuOptionById(@PathVariable Long restaurantId,
+                                                           @PathVariable Long menuId,
+                                                        @PathVariable Long menuOptionId) {
+        MenuOption menuOption = service.getMenuOptionById(restaurantId, menuId, menuOptionId);
+        return new ResponseEntity<>(menuOption, HttpStatus.OK);
+    }
+
+    @PutMapping("/{restaurantId}/menu/items/{menuId}/options/{menuOptionId}")
+    public ResponseEntity<MenuOption> updateMenuOption(@PathVariable Long restaurantId, @PathVariable Long menuId,
+                                                       @PathVariable Long menuOptionId,
+                                           @Valid @RequestBody MenuOptionRequest menuOption) {
+        MenuOption updatedMenuOption = service.updateMenuOption(restaurantId, menuId, menuOptionId, menuOption);
+        return new ResponseEntity<>(updatedMenuOption, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{restaurantId}/menu/items/{menuId}/options/{menuOptionId}")
+    public ResponseEntity<MenuOption> deleteMenuOption(@PathVariable Long restaurantId, @PathVariable Long menuId,
+                                                       @PathVariable Long menuOptionId) {
+        MenuOption deletedMenuOption = service.deleteMenuOption(restaurantId, menuId, menuOptionId);
+        return new ResponseEntity<>(deletedMenuOption, HttpStatus.OK);
+    }
+
+    /* ----------------------- For query or filter purposes ----------------------- */
     @GetMapping("/name/{name}")
     public Restaurant getRestaurantByName(@PathVariable String name) {
         return service.getRestaurantByName(name);
--- src/main/java/ku/cs/kuwongnai/restaurant/service/RestaurantService.java
+++ src/main/java/ku/cs/kuwongnai/restaurant/service/RestaurantService.java
@@ -1,17 +1,17 @@
 package ku.cs.kuwongnai.restaurant.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import ku.cs.kuwongnai.restaurant.entity.Menu;
+import ku.cs.kuwongnai.restaurant.entity.MenuOption;
 import ku.cs.kuwongnai.restaurant.entity.Restaurant;
+import ku.cs.kuwongnai.restaurant.model.MenuOptionRequest;
 import ku.cs.kuwongnai.restaurant.model.MenuRequest;
 import ku.cs.kuwongnai.restaurant.model.RestaurantRequest;
+import ku.cs.kuwongnai.restaurant.repository.MenuOptionRepository;
 import ku.cs.kuwongnai.restaurant.repository.MenuRepository;
 import ku.cs.kuwongnai.restaurant.repository.RestaurantRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -25,18 +25,26 @@ public class RestaurantService {
     private MenuRepository menuRepository;
 
     @Autowired
-    private ModelMapper modelMapper;
+    private MenuOptionRepository menuOptionRepository;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private ModelMapper modelMapper;
+
+    public List<Restaurant> getAllRestaurant() {
+        return restaurantRepository.findAll();
+    }
 
     public Restaurant createRestaurant(RestaurantRequest restaurant) {
         Restaurant record = modelMapper.map(restaurant, Restaurant.class);
         return restaurantRepository.save(record);
     }
 
+    public Restaurant getRestaurantById(Long id) {
+        return restaurantRepository.findById(id).get();
+    }
+
     public Restaurant updateRestaurant(Long id, RestaurantRequest requestBody) {
-        Restaurant record = restaurantRepository.findById(id).get();
+        Restaurant record = getRestaurantById(id);
         record.setName(requestBody.getName());
         record.setLocation(requestBody.getLocation());
         record.setDescription(requestBody.getDescription());
@@ -49,26 +57,97 @@ public class RestaurantService {
     }
 
     public Restaurant deleteRestaurant(Long id) {
-        Restaurant record = restaurantRepository.findById(id).get();
+        Restaurant record = getRestaurantById(id);
         restaurantRepository.deleteById(id);
         return record;
     }
 
+    /* ----------------------- For Menu ----------------------- */
+
+    public List<Menu> getMenus(Long id) {
+        Restaurant restaurant = getRestaurantById(id);
+        return restaurant.getMenus();
+    }
+
     public Menu createMenu(Long id, MenuRequest menu) {
         Menu record = modelMapper.map(menu, Menu.class);
-        Restaurant restaurant = restaurantRepository.findById(id).get();
+        Restaurant restaurant = getRestaurantById(id);
         record.setRestaurant(restaurant);
         return menuRepository.save(record);
     }
 
-    public List<Restaurant> getAllRestaurant() {
-        return restaurantRepository.findAll();
+    public Menu getMenuById(Long restaurantId, Long menuId) {
+        Menu record = menuRepository.findById(menuId).get();
+
+        // Check if menu belongs to specified restaurant
+        if (!record.getRestaurant().getId().equals(restaurantId)) {
+            throw new RuntimeException("This menu doesn't belong to specified restaurant");
+        }
+
+        return record;
     }
 
-    public Restaurant getRestaurantById(Long id) {
-        return restaurantRepository.findById(id).get();
+    public Menu updateMenu(Long restaurantId, Long menuId, MenuRequest requestBody) {
+        Menu record = getMenuById(restaurantId, menuId);
+
+        // Update menu
+        record.setName(requestBody.getName());
+        record.setDescription(requestBody.getDescription());
+        record.setImage(requestBody.getImage());
+        record.setPrice(requestBody.getPrice());
+        record.setCategory(requestBody.getCategory());
+        return menuRepository.save(record);
     }
 
+    public Menu deleteMenu(Long restaurantId, Long menuId) {
+        Menu record = getMenuById(restaurantId, menuId);
+        menuRepository.deleteById(menuId);
+        return record;
+    }
+
+    /* ----------------------- For Menu Option ----------------------- */
+
+    public List<MenuOption> getMenuOptions(Long restaurantId, Long menuId) {
+        Menu menu = getMenuById(restaurantId, menuId);
+        return menu.getMenuOptions();
+    }
+
+    public MenuOption createMenuOption(Long restaurantId, Long menuId, MenuOptionRequest menuOption) {
+        MenuOption record = modelMapper.map(menuOption, MenuOption.class);
+        Menu menu = getMenuById(restaurantId, menuId);
+        record.setMenu(menu);
+        return menuOptionRepository.save(record);
+    }
+
+    public MenuOption getMenuOptionById(Long restaurantId, Long menuId, Long menuOptionId) {
+        MenuOption record = menuOptionRepository.findById(menuOptionId).get();
+
+        // Check if menu belongs to specified restaurant
+        Menu menu = getMenuById(restaurantId, menuId);
+
+        // Check if menu option belongs to specified menu
+        if (!record.getMenu().getId().equals(menu.getId())) {
+            throw new RuntimeException("This menu option doesn't belong to specified menu");
+        }
+
+        return record;
+    }
+
+    public MenuOption updateMenuOption(Long restaurantId, Long menuId, Long menuOptionId, MenuOptionRequest requestBody) {
+        MenuOption record = getMenuOptionById(restaurantId, menuId, menuOptionId);
+        record.setName(requestBody.getName());
+        record.setPrice(requestBody.getPrice());
+        record.setCategory(requestBody.getCategory());
+        return menuOptionRepository.save(record);
+    }
+
+    public MenuOption deleteMenuOption(Long restaurantId, Long menuId, Long menuOptionId) {
+        MenuOption record = getMenuOptionById(restaurantId, menuId, menuOptionId);
+        menuOptionRepository.deleteById(menuOptionId);
+        return record;
+    }
+
+    /* ----------------------- For query or filter purposes ----------------------- */
     public Restaurant getRestaurantByName(String name) {
         return restaurantRepository.findByName(name);
     }
